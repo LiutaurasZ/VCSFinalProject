ecdcbc73e179ebb067db9a01968a2ff476502f11
Liutauras <liutauras.zilovas@gmail.com>
2022-10-15 16:00:53 +0300

    Add TestBase class

---
+++ src/test/java/lt/liutauras/tests/TestBase.java
@@ -0,0 +1,18 @@
+package lt.liutauras.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase {
+
+    @BeforeMethod
+    public void setUp() {
+        Common.setUpDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        Common.closeDriver();
+    }
+
+}
