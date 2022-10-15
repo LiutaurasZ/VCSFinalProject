4d04bf07a9287eaf57d4656f0025f8b80dd142e9
Liutauras <liutauras.zilovas@gmail.com>
2022-10-15 19:01:23 +0300

    Add waitForElementWithVisibilityChange() to Common

--- src/test/java/lt/liutauras/pages/Common.java
+++ src/test/java/lt/liutauras/pages/Common.java
@@ -3,6 +3,10 @@ package lt.liutauras.pages;
 import lt.liutauras.utils.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Common {
 
@@ -35,6 +39,10 @@ public class Common {
         getElement(locator).click();
     }
 
+    public static void waitForElementWithVisibilityChange(By locator) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(8));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
 
 
 
