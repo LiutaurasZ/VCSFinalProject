b00d03f5b4f515bf08f2611edc4e4776c7f2f06a
Liutauras <liutauras.zilovas@gmail.com>
2022-10-15 21:53:31 +0300

    Add and implement TestListener class

--- src/test/java/lt/liutauras/tests/TestBase.java
+++ src/test/java/lt/liutauras/tests/TestBase.java
@@ -1,9 +1,13 @@
 package lt.liutauras.tests;
 
 import lt.liutauras.pages.Common;
+import lt.liutauras.utils.TestListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+
+@Listeners(TestListener.class)
 public class TestBase {
 
     @BeforeMethod
