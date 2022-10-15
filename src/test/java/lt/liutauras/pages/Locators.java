a1b4fd0f8583045f05f39b6a446c6c387f291a90
Liutauras <liutauras.zilovas@gmail.com>
2022-10-15 17:33:06 +0300

    Add Login class to Locators

--- src/test/java/lt/liutauras/pages/Locators.java
+++ src/test/java/lt/liutauras/pages/Locators.java
@@ -1,13 +1,19 @@
 package lt.liutauras.pages;
 
+import org.openqa.selenium.By;
+
 public class Locators {
 
     public static class Pegasas {
 
+        public static class Login {
+            public static By buttonConfirm = By.xpath(
+                    "//a[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']");
+            public static By formPrivacyConfirmation = By.xpath("");
 
+
+        }
 
 
     }
-
-
 }
