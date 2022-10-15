aec5b4f6615ee876a57e0100dd4fe9bcf2e07c2d
Liutauras <liutauras.zilovas@gmail.com>
2022-10-15 19:44:55 +0300

    Add new variables to Login class

--- src/test/java/lt/liutauras/pages/Locators.java
+++ src/test/java/lt/liutauras/pages/Locators.java
@@ -7,11 +7,14 @@ public class Locators {
     public static class Pegasas {
 
         public static class Login {
-            public static By buttonConfirm = By.xpath(
+            public static By buttonAllowAll = By.xpath(
                     "//a[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']");
-            public static By formPrivacyConfirmation = By.xpath("");
+            public static By formPrivacyConfirmation = By.xpath("//div[@id='CybotCookiebotDialog']");
 
-
+            public static By buttonClose = By.xpath(
+                    "//button[@id='omnisend-form-633c09919a4d16cd5e1876af-close-action']");
+            public static By formNaujiemsPrenumeratoriams = By.xpath(
+                    "//form[@id='omnisend-form-633c09919a4d16cd5e1876af-submit-form']");
         }
 
 
