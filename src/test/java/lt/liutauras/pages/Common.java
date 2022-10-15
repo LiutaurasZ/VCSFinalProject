1bb99e11b424e6a74642531c2c186d9bcf77f121
Liutauras <liutauras.zilovas@gmail.com>
2022-10-15 16:01:20 +0300

    Add Common class

---
+++ src/test/java/lt/liutauras/pages/Common.java
@@ -0,0 +1,30 @@
+package lt.liutauras.pages;
+
+public class Common {
+
+
+    public static void setUpDriver() {
+        Driver.setDriver();
+    }
+
+    public static void openUrl(String url) {
+        Driver.getDriver().get(url);
+    }
+
+    public static void closeDriver() {
+        Driver.close();
+    }
+
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+}
