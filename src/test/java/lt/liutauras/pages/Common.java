2fa059c7ecb3496ff0e1cba4e03bc672db5ba8f4
Liutauras <liutauras.zilovas@gmail.com>
2022-10-17 13:56:35 +0300

    Add clearInputField and sendKeyboardKeysToElement

--- src/test/java/lt/liutauras/pages/Common.java
+++ src/test/java/lt/liutauras/pages/Common.java
@@ -2,15 +2,17 @@ package lt.liutauras.pages;
 
 import lt.liutauras.utils.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class Common {
-
 
     public static void setUpDriver() {
         Driver.setDriver();
@@ -53,17 +55,38 @@ public class Common {
     public static void sendKeysToElement(String keys, By locator) {
         getElement(locator).sendKeys(keys);
     }
+
     public static String getElementText(By locator) {
         return getElement(locator).getText();
     }
 
-    public static void doubleClickByActions(By locator) {
-        WebElement element = getElement(locator);
 
-        Actions action = new Actions(Driver.getDriver());
-        action.moveToElement(element);
-        action.doubleClick();
-        action.perform();
+    public static void selectOptionByValue(By locator, String value) {
+        WebElement webElement = getElement(locator);
+        Select selectElement = new Select(webElement);
+        selectElement.selectByValue(value);
     }
 
+    public static void clearInputField(By locator) {
+        getElement(locator).clear();
+    }
+
+    public static void sendKeyboardKeysToElement(Keys value, By locator) {
+        getElement(locator).sendKeys(value);
+    }
+
+    public static void waitForElementAttributeToBe(By locator, String attribute, String value)  {
+
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeToBe(locator, attribute, value));
+    }
+
+    public static void waitForElementIsAccesible(By locator) {
+      //  WebElement webElement = getElement(locator);
+        FluentWait wait = new FluentWait(Driver.getDriver());
+        wait.withTimeout(Duration.ofSeconds(7));
+        wait.pollingEvery(Duration.ofMillis(500));
+        wait.ignoring(NoSuchElementException.class);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+    }
 }
